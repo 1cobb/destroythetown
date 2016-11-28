960a803a16eedc9d788af420f9f38f90d9b63c23
1cobb <jam.cobb@icloud.com>
2016-11-28 16:28:45 -0500

    Add files via upload

---
+++ ASCIIAssignment.swift
@@ -0,0 +1,128 @@
+//
+//  ASCIIAssignment.swift
+//  
+//
+//  Created by James Cobb on 11/28/16.
+//
+//
+
+import Foundation
+import Darwin
+
+func clearScreen() {
+    Process.launchedProcess(launchPath: "/usr/bin/clear", arguments: []).waitUntilExit()
+}
+
+clearScreen()
+print("GAME IS ON THE LINE...")
+
+sleep(1)
+clearScreen()
+
+print("Eagles Have the ball with seconds left")
+
+sleep(2)
+clearScreen()
+
+print("       Last Play... Packed Stadium      ")
+print("                                        ")
+print(" \\ 894389hbfeoifqewn00000jsfkjnkjjk // ")
+print("  \\ 898943h8rnfkfdsajknfdsahjfiihf //  ")
+print("   \\ 843983498hgfjhsjkdjhkfshjkjk //   ")
+print("    \\ 4kjkfnwonewifnwofnoofjfewe //    ")
+print("     \\ 29473904792fhwhfkfwjwoif //     ")
+print("      \\ d47729739fhwfsjnbwnwi9 //      ")
+print("                                        ")
+print("           \0/    \0/    \0/            ")
+print("            |      |      |             ")
+print("          // \\  // \\  // \\           ")
+print("----------------------------------------")
+
+sleep(3)
+clearScreen()
+
+print("    4th Down!       ")
+print("                    ")
+print(" |---------------|  ")
+print(" |               |  ")
+print(" |    4    4     |  ")
+print(" |    4    4     |  ")
+print(" |    444444     |  ")
+print(" |         4     |  ")
+print(" |         4     |  ")
+print(" |               |  ")
+print(" |---------------|  ")
+sleep(3)
+clearScreen()
+
+print("     Ball is on the 20 yard line    ")
+print("                                    ")
+print("                                    ")
+print("                < >< >              ")
+print("              <        >            ")
+print("            <            >          ")
+print("          <      ####      >        ")
+print("            <            >          ")
+print("              <        >            ")
+print("                < >< >              ")
+print("------------------------------------")
+print("10               20               30")
+
+sleep(3)
+clearScreen()
+
+print("Quarterback drops back to throw...")
+print("                                  ")
+print("      <<>>                        ")
+print("        \\                        ")
+print("         \\ 00 //                 ")
+print("            ||                    ")
+print("            ||                    ")
+print("            ||                    ")
+print("         //    \\                 ")
+print("        //      \\                ")
+clearScreen()
+
+sleep(3)
+clearScreen()
+
+print("Ball is in the Air")
+print("                                            |       |  ")
+print("                                            |       |  ")
+print("                      <>                    |       |  ")
+print("            <>                  <>          |_______|  ")
+print("   <>                                    <>     |      ")
+print("  \\0//     \\0// \\0//       \\0//   \\0//     |      ")
+print("   |||       |||   |||         |||     |||      |      ")
+print("  // \\     // \\ // \\       // \\   // \\     |      ")
+print(" ------------------------------------------------------")
+
+sleep(3)
+clearScreen()
+
+print("TOUCHDOWN!!!")
+
+clearScreen()
+
+print("       Eagles Win         ")
+print("                          ")
+print("  www                 www ")
+print("   www               www  ")
+print("    www             www   ")
+print("     www   ww ww   www    ")
+print("      www ww   ww www     ")
+print("       wwww     wwww      ")
+print("        www     www       ")
+
+
+
+
+
+
+
+
+
+
+
+
+
